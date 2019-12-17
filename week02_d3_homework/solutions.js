eacfe8aab329537c28a9ecde5492127b6925e329
Axel Campero <axelcampero@Axels-MBP.fios-router.home>
2019-12-17 15:51:41 -0500

    verbal questions completed

---
+++ week02_d3_homework/solutions.js
@@ -0,0 +1,9 @@
+///////////////////////
+//Verbal Questions/////
+///////////////////////
+/*1.What is the difference between a parameter and an argument?
+A parameter is a variable in a method definition and an argument  
+is the actual value of this variable that gets passed to function.
+2.Within a function, what is the difference between return and console.log?
+Return is a statement that allows a function to output a value back to where 
+it was called before. Console. log is a function that lets us inspect values for debugging purposes.*/
